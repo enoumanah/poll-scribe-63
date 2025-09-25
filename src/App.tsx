ff384854ce8e76e604a684e7f2952cf8e0847046
enoumanah <141334494+enoumanah@users.noreply.github.com>
2025-09-25 13:20:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
@@ -19,6 +19,7 @@ import PollView from "./pages/PollView";
 import MyActivity from "./pages/MyActivity";
 import Privacy from "./pages/Privacy";
 import NotFound from "./pages/NotFound";
+import PollResultsView from "./pages/PollResultsView";
 
 const queryClient = new QueryClient();
 
@@ -49,29 +50,49 @@ const App = () => (
               <Route path="/login" element={<Login />} />
               <Route path="/privacy" element={<Privacy />} />
               <Route path="/share/:shareLink" element={<PollView />} />
-              
+
               {/* Protected Routes */}
-              <Route path="/dashboard" element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              } />
-              <Route path="/create" element={
-                <ProtectedRoute>
-                  <CreatePoll />
-                </ProtectedRoute>
-              } />
-              <Route path="/polls/:id" element={
-                <ProtectedRoute>
-                  <PollView />
-                </ProtectedRoute>
-              } />
-              <Route path="/my-activity" element={
-                <ProtectedRoute>
-                  <MyActivity />
-                </ProtectedRoute>
-              } />
-              
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/create"
+                element={
+                  <ProtectedRoute>
+                    <CreatePoll />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/polls/:id"
+                element={
+                  <ProtectedRoute>
+                    <PollView />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/my-activity"
+                element={
+                  <ProtectedRoute>
+                    <MyActivity />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/polls/:id/results"
+                element={
+                  <ProtectedRoute>
+                    <PollResultsView />
+                  </ProtectedRoute>
+                }
+              />
+
               {/* Catch-all route - must be last */}
               <Route path="*" element={<NotFound />} />
             </Routes>
